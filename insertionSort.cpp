60cc4ee1ea0a6333150114589dc2b84df847ea2d
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2023-09-14 01:26:19 +0530

    fixed code

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,22 +1,37 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-// Function to sort an array using insertion sort
-void insertionSort(vectoe<int>& arr, int n)
-{
-    int i, key, j;
-    for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
- 
-        // Move elements of arr[0..i-1],
-        // that are greater than key,
-        // to one position ahead of their
-        // current position
+void insertionSort(vector<int> &arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
-            j = j - 1;
+            j--;
         }
+        
         arr[j + 1] = key;
     }
+}
+
+int main() {
+    vector<int> arr = {12, 11, 13, 5, 6};
+
+    cout << "Original array: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+
+    insertionSort(arr);
+
+    cout << "\nSorted array: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+
+    return 0;
 }
